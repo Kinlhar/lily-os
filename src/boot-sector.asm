3307bb81319423a4285326e3e9042f102d8b303e
Sylvain Fortin <sylvain.fortin@hotmail.com>
2017-12-15 09:54:32 -0500

    Memory, addresses and labels exercise.

--- src/boot-sector.asm
+++ src/boot-sector.asm
@@ -1,29 +1,32 @@
 ;
-; A simple boot sector that prints a message to the screen using a BIOS routine.
+; A simple boot sector program that demonstrates addressing.
 ;
-    mov ah, 0x0e    ; int 10/ ah = 0eh -> scrolling teletype BIOS routine
+    [org 0x7c00]
+    mov ah, 0x0e ; int 10/ah = 0eh -> scrolling teletype BIOS routine
 
-    mov al, 'H'
-    int 0x10
+    ; First attempt
+    mov al, the_secret
+    int 0x10 ; Does this print an X?
 
-    mov al , 'e'
-    int 0x10
+    ; Second attempt 
+    mov al, [the_secret] 
+    int 0x10 ; Does this print an X?
 
-    mov al , 'l'
-    int 0x10
+    ; Third attempt 
+    mov bx, the_secret 
+    add bx, 0x7c00 
+    mov al, [bx] 
+    int 0x10 ; Does this print an X?
 
-    mov al , 'l'
-    int 0x10
+    ; Fourth attempt
+    mov al, [0x7c1e] 
+    int 0x10 ; Does this print an X?
 
-    mov al , 'o'
-    int 0x10
+    jmp $ ; Jump forever.
 
-    jmp $           ; Jump to the current address ( i.e. forever ).
+the_secret:
+    db "X"
 
-;
 ; Padding and magic BIOS number.
-;
-    times 510 -( $ - $$ ) db 0  ; Pad the boot sector out with zeros
-
-    dw 0xaa55                   ; Last two bytes form the magic number ,
-                                ; so BIOS knows we are a boot sector.
+    times 510-($-$$) db 0
+    dw 0xaa55
